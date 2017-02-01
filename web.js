4e264cab8b02c1f342530bbfc8191f0073cdbf35
Nishit <nishitm@cse.iitk.ac.in>
2017-02-02 01:20:47 +0530

    error solved

--- web.js
+++ web.js
@@ -5,11 +5,11 @@ var p = process.env.PORT || 8000;
 app.set('view engine','ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/',fun(req,res){
+app.get('/',function(req,res){
 	res.render('index');
 	});
 
-app.listen(p,fun(){
+app.listen(p,function(){
 	console.log("yoohoo!!! App is Running on port "+p);
 	});
 	
