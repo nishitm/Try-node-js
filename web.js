947c6e93b1bd78a0c8187b107e0c59d0167c2d8f
Nishit <nishitm@cse.iitk.ac.in>
2017-02-02 00:43:09 +0530

    web file

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var p = process.env.PORT || 8000;
+
+app.set('view engine','ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',fun(req,res){
+	res.render('index');
+	});
+
+app.listen(p,fun(){
+	console.log("yoohoo!!! App is Running on port '+p);
+	});
+	
